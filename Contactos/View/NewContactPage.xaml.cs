18ed7d53e4cd4b25107e45f8c7ec26dc8fcaafdf
lalorosas <lalorosas@outlook.com>
2019-01-14 12:27:22 -0700

    NewContactVM agregado con funcionalidad de actualizar e insertar

--- Contactos/View/NewContactPage.xaml.cs
+++ Contactos/View/NewContactPage.xaml.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using Contactos.Model;
+using Contactos.ViewModel;
 using Xamarin.Forms;
 
 namespace Contactos.View
 {
     public partial class NewContactPage : ContentPage
     {
-        bool isEditing = false;
-        Contact contact = null;
+        NewContactVM viewModel;
 
         public NewContactPage()
         {
@@ -19,48 +19,10 @@ namespace Contactos.View
         {
             InitializeComponent();
 
-            isEditing = true;
-            this.contact = contact;
+            viewModel = Resources["vm"] as NewContactVM;
 
-            nameEntry.Text = contact.Name;
-            lastnameEntry.Text = contact.LastName;
-            emailEntry.Text = contact.Email;
-            phoneEntry.Text = contact.Phone;
-        }
-
-        void Handle_Clicked(object sender, System.EventArgs e)
-        {
-            //TODO: Guardar en tabla Contacto
-            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DatabasePath))
-            {
-                conn.CreateTable(typeof(Contact));
-
-                int filasModificadas = 0;
-
-                if (!isEditing)
-                {
-                    filasModificadas = conn.Insert(new Contact
-                    {
-                        Name = nameEntry.Text,
-                        LastName = lastnameEntry.Text,
-                        Email = emailEntry.Text,
-                        Phone = phoneEntry.Text
-                    });
-                }
-                else
-                {
-                    contact.Name = nameEntry.Text;
-                    contact.LastName = lastnameEntry.Text;
-                    contact.Email = emailEntry.Text;
-                    contact.Phone = phoneEntry.Text;
-                    filasModificadas = conn.Update(contact);
-                }
-
-                if (filasModificadas > 0)
-                    Navigation.PopAsync();
-                else
-                    DisplayAlert("Error", "Error al modificar", "Ok");
-            }
+            viewModel.IsEditing = true;
+            viewModel.Contacto = contact;
         }
     }
 }
---
+++ Contactos/ViewModel/NewContactVM.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Contactos.Model;
+using SQLite;
+using Xamarin.Forms;
+
+namespace Contactos.ViewModel
+{
+    public class NewContactVM : INotifyPropertyChanged
+    {
+        private bool isEditing;
+        public bool IsEditing
+        {
+            get { return isEditing; }
+            set
+            {
+                isEditing = value;
+                OnPropertyChanged("IsEditing");
+            }
+        }
+
+        private Contact contacto;
+        public Contact Contacto
+        {
+            get { return contacto; }
+            set
+            {
+                contacto = value;
+                Name = contacto.Name;
+                Lastname = contacto.LastName;
+                Email = contacto.Email;
+                Phone = contacto.Phone;
+                OnPropertyChanged("Contacto");
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private string lastname;
+        public string Lastname
+        {
+            get { return lastname; }
+            set
+            {
+                lastname = value;
+                OnPropertyChanged("Lastname");
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        private string phone;
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                phone = value;
+                OnPropertyChanged("Phone");
+            }
+        }
+
+        public ICommand SaveContactCommand { get; set; }
+
+        public NewContactVM()
+        {
+            IsEditing = false;
+            SaveContactCommand = new Command(SaveContact);
+        }
+
+        void SaveContact(object parameter)
+        {
+            int filasModificadas = 0;
+
+            if (!IsEditing)
+            {
+                Contact contact = new Contact
+                {
+                    Name = Name,
+                    LastName = Lastname,
+                    Email = Email,
+                    Phone = Phone
+                };
+
+                using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+                {
+                    conn.CreateTable<Contact>();
+                    filasModificadas = conn.Insert(contact);
+                }
+            }
+            else
+            {
+                Contacto.Name = Name;
+                Contacto.LastName = Lastname;
+                Contacto.Email = Email;
+                Contacto.Phone = Phone;
+
+                using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+                {
+                    conn.CreateTable<Contact>();
+                    filasModificadas = conn.Update(Contacto);
+                }
+            }
+
+            if (filasModificadas > 0)
+                App.Current.MainPage.Navigation.PopAsync();
+            else
+                App.Current.MainPage.DisplayAlert("Error", "No se modificó la tabla", "Ok");
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
